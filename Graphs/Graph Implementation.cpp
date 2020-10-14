33701984d7ced5a2153147d82be20266d5270962
dnigam1 <xaviercrood82@gmail.com>
2020-10-14 21:54:43 +0530

    updation

--- Graphs/Graph Implementation.cpp
+++ Graphs/Graph Implementation.cpp
@@ -6,5 +6,36 @@ class graph
 	int v;
 	list<int > *l;
 	public:
-		
+		graph(int v)
+		{
+			this->v=v;
+			l=new list<int>[v];  // l is pointer to array list
+		}
+		void addedge(int x,int y)
+		{
+			l[x].push_back(y);
+			l[y].push_back(x);
+		}
+		void printlist()
+		{
+			for(int i=0;i<v;i++)
+			{
+				cout<<" vertex "<<i<<"->";
+				for(int nbr:l[i])
+				{
+					cout<<nbr<<",";
+				}
+				cout<<endl;
+			}
+		}
 };
+int main()
+{
+	graph g(4);
+	g.addedge(0,1);
+	g.addedge(0,2);
+	g.addedge(2,3);
+	g.addedge(1,2);
+	printlist();
+	return 0;
+}
