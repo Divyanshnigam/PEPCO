02bc71a46461d436845152fd2a5527f75bc72f10
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-01 01:07:32 +0530

    commited

---
+++ LEVEL-UP/RECURSION-LEVEL-UP/Solve_Sudoku.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void solve(int *board,int i,int j)
+{
+	int ni=
+}
+int main()
+{
+	int board[10][10];
+	for(int i=0;i<10;i++)
+	{
+		for (int j=0;j<10;j++)
+		{
+			cin>>board[i][j];
+		}
+	}
+	solve(board,0,0);
+}
