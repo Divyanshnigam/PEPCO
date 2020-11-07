d5cd9d10abcfc063edb02abcb6e67d69b3119ff7
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-08 00:29:51 +0530

    commited

---
+++ LEVEL-UP/RECURSION-LEVEL-UP/Coin Change - Combinations - 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void solve(int *a,int n,int total,string asf,int sum,int i)
+{
+    if(i==n or sum>total)
+    {
+        if(sum==total)
+        {
+            cout<<asf<<"."<<endl;
+        }
+        return;
+    }    
+    solve(a,n,total,asf + to_string(a[i]) + "-" ,sum+a[i],i);
+    solve(a,n,total,asf ,sum,i+1);    
+   
+}
+
+int main() 
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int total;
+    cin>>total;
+    solve(a,n,total,"",0,0);
+}
+
