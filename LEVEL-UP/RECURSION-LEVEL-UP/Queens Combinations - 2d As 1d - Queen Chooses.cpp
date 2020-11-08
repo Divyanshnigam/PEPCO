ae73c4f60de1c5298b9e0041267678612ba79bd7
Divyanshnigam <divyanshnigam1612@gmail.com>
2020-11-09 03:37:58 +0530

    commited

---
+++ LEVEL-UP/RECURSION-LEVEL-UP/Queens Combinations - 2d As 1d - Queen Chooses.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void queensCombination(int qsf,int tq,bool chess[][10],int last_cellno)
+{
+    if(qsf==tq)
+    {
+        for(int row=0;row<tq;row++)
+        {
+            for(int col=0;col<tq;col++)
+            {
+                if(chess[row][col]==true)
+                {
+                    cout<<"q\t";
+                }
+                else
+                {
+                    cout<<"-\t";
+                }
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+        return;
+    }
+    for(int  cell =last_cellno + 1,cell<tq*tq;cell++)
+    {
+        int row=cell/tq;
+        int col=cell%tq;
+
+        chess[row][col]=true;
+        queensCombination(qsf+1,tq,chess,cell);
+        chess[row][col]=false;
+    }    
+}
+int main() 
+{
+    int n;
+    cin>>n;
+    bool chess[n][n];
+    queensCombination(0,n,chess,0,-1);
+}
+
