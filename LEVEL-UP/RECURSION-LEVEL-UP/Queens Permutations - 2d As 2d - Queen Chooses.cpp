4c8b6acdffe94b34520a0cf6f3b4153789e257c9
Dhruv Trehan <46952155+DhruvTrehan@users.noreply.github.com>
2020-11-11 14:30:17 +0530

    Update Queens Permutations - 2d As 2d - Queen Chooses.cpp
    Updated the passing of 2d array

--- LEVEL-UP/RECURSION-LEVEL-UP/Queens Permutations - 2d As 2d - Queen Chooses.cpp
+++ LEVEL-UP/RECURSION-LEVEL-UP/Queens Permutations - 2d As 2d - Queen Chooses.cpp
@@ -1,47 +1,55 @@
-#include <iostream>
-#include <cstring>
+#include<iostream>
 using namespace std;
-void queensPermutations(int qsf,int tq,int chess_board[][10])
+void swap(int bc, int up, int **arr)
 {
-    if(qsf==tq)
+    //base case
+    if(bc == up)
     {
-        for(int i=0;i<tq;i++)
+        for(int i = 0 ; i < up ; i++)
         {
-            for(int j=0;j<tq;j++)
+            for(int j = 0 ; j < up ; j++)
             {
-                if(chess_board[i][j]==0)
+                if(arr[i][j] == 0)
                 {
                     cout<<"-\t";
                 }
                 else
                 {
-                    cout<<"q"<<chess_board[i][j]<<"\t";
-                }            
+                    cout<<"q"<<arr[i][j]<<"\t";
+                }
+                
             }
             cout<<endl;
         }
         cout<<endl;
         return;
     }
-
-    for(int i=0;i<tq;i++)
+    for(int i = 0 ; i < up ; i++)
     {
-        for(int j=0;j<tq;j++)
+        for(int j = 0 ; j < up ; j++)
         {
-            if(chess_board[i][j]==0)
+            if(arr[i][j] == 0)
             {
-                chess_board[i][j]=1;
-                queensPermutations(qsf+1,tq,chess_board);
-                chess_board[i][j]=0;
+                arr[i][j] = bc + 1;
+                swap(bc+1,up,arr);
+                arr[i][j] = 0;
             }
         }
     }
 }
-int main() 
+int main()
 {
     int n;
     cin>>n;
-    int chess_board[n][n]={0};
-    queensPermutations(0,n,chess_board);
+    int** arr = new int*[n+1];
+    for(int i = 0 ; i < n ; i++)
+    {
+        arr[i] = new int[n+1];
+        for(int j = 0 ; j < n ; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    swap(0,n,arr);
+    
 }
-
