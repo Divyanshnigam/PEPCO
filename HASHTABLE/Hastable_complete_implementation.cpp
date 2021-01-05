0aee2f7f628a828feffb071315ad794813b147a5
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-01-05 21:00:33 +0530

    insertion implementated

--- HASHTABLE/Hastable_complete_implementation.cpp
+++ HASHTABLE/Hastable_complete_implementation.cpp
@@ -32,11 +32,25 @@ class Hashtable
 	node<T>** table;  // pointer to array of pointers
 	int current_size;
 	int table_size;
+	
+	int hashfn(string key)
+	{
+		int idx=0;
+		int p=1;
+		for(int j=0;j<key.length();j++)
+		{
+			idx=idx+(key[j]*p)%table_size;    //  The key itself will give the index.
+            idx=idx%table_size;      // moduloe every time to abide segmenation fault..
+            p=(p*27)%table_size; // every time the p is updated with higher power (p is a distinguished prime no.)
+		}
+        return idx;
+	}
+	
 	public:
 		Hashtable(int ts=7)
 		{
-			table_size=ts;
-			table = new node<T>*[table_size];  //dynamic..
+			table_size=ts;  
+			table = new node<T>*[table_size];  //dynamic.. a random node table to store next to another table.
 			current_size=0;
 			for(int i=0;i<table_size;i++)
 			{
@@ -44,6 +58,19 @@ class Hashtable
 			}
 		}
 }
+
+void insert(string key ,T value)
+{
+	int idx=hashfn(key);  
+    node<T>*n=new node<T> (key,value);
+    //Inserting at the head of the linked list with id=idx
+    n->next=table[idx];
+    table[idx]=n;
+    current_size++;
+    //rehash
+	
+}
+
 
 
 int main()
