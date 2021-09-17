f8145bf86074f6c2d5e7f3ceda3105fd04ec2683
Divyanshnigam <divyanshnigam1612@gmail.com>
2021-09-17 11:20:02 +0530

    frequ

--- Hashing with MAP STL/Frequent_elements.cpp
+++ Hashing with MAP STL/Frequent_elements.cpp
@@ -2,32 +2,20 @@
 using namespace std;
 void printfre(vector<int> a, int n)
 {
-    for(int i=0;i<n;i++)
+    unordered_map<int ,int >m;
+    for(auto x: a)
     {
-        bool flag =false;
-        for(int j=0;j<i;i++)
-        {
-            if(a[i]==a[j])
-            {
-                flag=true;
-                break;
-            }
-        }
-        if(flag==true)
-        {
-            continue;
-        }
-        int freq=1;
-        for(int j=i+1;j<n;j++)
-        {
-            if(a[i]==a[j])
-                freq++;
-        }
-        cout<<a[i]<<" "<<freq<<endl;
+        m[x]++;
     }
+
+    for(auto e:m)
+    {
+        cout<<e.first<<" "<<e.second<<endl;
+    }
+
 }
 int main()
 {
-    vector<int> a{10,20,20,30,40};
+    vector<int> a{10,20,20,30,10};
     printfre(a,a.size());
 }
